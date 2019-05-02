a40f3e372c3d9725bc9df0c9efe3dc99ceb6fd13
Reina Ewing <reina.castro3@gmail.com>
2019-05-02 15:26:50 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,27 @@
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def valid_move?(board, index)
+   index.between?(0, 8) && !(position_taken?(board, index))
+  end
+  
+def position_taken?(board, index)
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    return false
+  else
+    return true
+  end
+end 
+
+def input_to_index(board, index)
+  
+end 
+  
